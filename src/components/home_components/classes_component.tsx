76a6428d2c1c2bcc07025b2fdac9eab22e4e88d4
Arockiaraj19 <punithaarockiaraj1999@gmail.com>
2024-04-02 22:43:16 +0530

    home page traning mode updated

--- src/components/home_components/classes_component.tsx
+++ src/components/home_components/classes_component.tsx
@@ -1,38 +1,21 @@
 import React, { Fragment, useState,useEffect } from 'react'
 import MainHeading from '../helpers/heading/main_heading'
 import { axiosPublic } from '@/common/axiosPublic';
-
-axiosPublic
+import useTrainingMode from '@/hooks/training_mode_hook';
 
 
 
 export default function ClassesComponent() {
-    const [data, setData] = useState<any[]>([]);
-    useEffect(() => {
   
-      fetchData();
-  
-    }, [])
-  
-    const fetchData = async () => {
-      try {
-        const result = await axiosPublic.get('/lms/service');
-        console.log("what is the result");
-        console.log(result.data);
-        setData(result.data.service);
-      } catch (error) {
-  
-      }
-    }
-
+const {trainingData:data, isLoading}=useTrainingMode();
     
     return (
         <main className="w-full grid grid-cols-4 gap-2">
 {
    data.map((e)=>{
-        return <div key={e.serviceId} className='p-4 bg-dark_blue border-2  border-blue rounded-md h-[450px] flex flex-col justify-center gap-10'>
-            <h3 className='text-xl font-bold text-center text-blue'>{e.serviceTitle}</h3>
-            <p className='text-lg text-white text-center mb-10'>{e.serviceDescription}</p>
+        return <div key={e.trainingModeId} className='p-4 bg-dark_blue border-2  border-blue rounded-md h-[300px] flex flex-col justify-center gap-10'>
+            <h3 className='text-xl font-bold text-center text-blue'>{e.trainingModeTitle}</h3>
+            <p className='text-lg text-white text-center mb-10'>{e.trainingModeDescription}</p>
         </div>
     })
 }

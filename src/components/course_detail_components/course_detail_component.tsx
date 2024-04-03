dbc8d5ac22a1954544a43673dc37caacc5fd6dbe
Arockiaraj19 <punithaarockiaraj1999@gmail.com>
2024-04-03 09:26:40 +0530

    course detail description added

--- src/components/course_detail_components/course_detail_component.tsx
+++ src/components/course_detail_components/course_detail_component.tsx
@@ -20,85 +20,85 @@ import useTrainingMode from '@/hooks/training_mode_hook';
 import { axiosPublic } from '@/common/axiosPublic';
 
 
-export default function CourseDetailContainer({data}:{data:any}) {
-    const { userData,} = useUserData();
-    const {trainingData}=useTrainingMode();
+export default function CourseDetailContainer({ data }: { data: any }) {
+    const { userData, } = useUserData();
+    const { trainingData } = useTrainingMode();
     const [isLoading, setLoading] = useState(false);
     const params = useParams();
-    const router=useRouter();
-    const entroll=async()=>{
-if(userData==null){
-    return alert("Before entrollment Please login");
-}
-try {
-    if (isLoading) {
-        return;
-    }
-    setLoading(true);
-    const result = await axiosPrivate.post('/lms/add-course-enrollment', {
-        "userId":  userData.userId,
-        "courseId" :parseInt(params.courseId[0]),
-       
-        "courseCostPlanId": data.CourseCostPlans[0].courseCostPlanId,
-        "enrollmentReference":"This is Test Enrollment",
-        "amount": data.CourseCostPlans[0].planPrice
-        });
-
-
-    setLoading(false);
-    window.open(`${result.data.gateway.url}&encRequest=${result.data.gateway.encRequest}&access_code=${result.data.gateway.access_code}`);
-    console.log(result.data);
-   
-
-} catch (error: any) {
-    setLoading(false);
-    console.log(error);
-    alert(error!.message);
-
-}
-    }
-
-    const handleDownload = async (courseId:any,courseName:any) => {
-        try {
-          // Make a GET request to the API endpoint that serves the file
-          const response = await axiosPublic.get('/lms/course-download', {
-
-params:{
-courseId:courseId
-},            responseType: 'blob' // This tells Axios to expect a binary response
-          });
-    
-          // Create a blob object from the response data
-          const blob = new Blob([response.data], { type: 'application/pdf' });
-
-          // Create a URL for the blob object
-          const url = window.URL.createObjectURL(blob);
-    
-          // Create a link element and click it to trigger the download
-          const link = document.createElement('a');
-          link.href = url;
-          link.download = `${courseName}.pdf`; // Specify the filename here
-          document.body.appendChild(link);
-          link.click();
-    
-          // Clean up: remove the link and revoke the URL
-          document.body.removeChild(link);
-          window.URL.revokeObjectURL(url);
-        } catch (error) {
-          console.error('Error downloading file:', error);
+    const router = useRouter();
+    const entroll = async () => {
+        if (userData == null) {
+            return alert("Before entrollment Please login");
         }
-      };
+        try {
+            if (isLoading) {
+                return;
+            }
+            setLoading(true);
+            const result = await axiosPrivate.post('/lms/add-course-enrollment', {
+                "userId": userData.userId,
+                "courseId": parseInt(params.courseId[0]),
+
+                "courseCostPlanId": data.CourseCostPlans[0].courseCostPlanId,
+                "enrollmentReference": "This is Test Enrollment",
+                "amount": data.CourseCostPlans[0].planPrice
+            });
+
+
+            setLoading(false);
+            window.open(`${result.data.gateway.url}&encRequest=${result.data.gateway.encRequest}&access_code=${result.data.gateway.access_code}`);
+            console.log(result.data);
+
+
+        } catch (error: any) {
+            setLoading(false);
+            console.log(error);
+            alert(error!.message);
+
+        }
+    }
+
+    const handleDownload = async (courseId: any, courseName: any) => {
+        try {
+            // Make a GET request to the API endpoint that serves the file
+            const response = await axiosPublic.get('/lms/course-download', {
+
+                params: {
+                    courseId: courseId
+                }, responseType: 'blob' // This tells Axios to expect a binary response
+            });
+
+            // Create a blob object from the response data
+            const blob = new Blob([response.data], { type: 'application/pdf' });
+
+            // Create a URL for the blob object
+            const url = window.URL.createObjectURL(blob);
+
+            // Create a link element and click it to trigger the download
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `${courseName}.pdf`; // Specify the filename here
+            document.body.appendChild(link);
+            link.click();
+
+            // Clean up: remove the link and revoke the URL
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            console.error('Error downloading file:', error);
+        }
+    };
     const [index, setIndex] = useState(0);
     return (
         <main className="w-full bg-primary_color flex-1 flex flex-col justify-start items-start">
 
             <div className="flex flex-row gap-1 items-center">
-                <p className="text-blue text-base font-medium" onClick={(e)=>{
+                <p className="text-blue text-base font-medium" onClick={(e) => {
                     router.back();
                     router.back();
                 }}>Home</p>
                 <ChevronRightIcon className="text-text_grey_one h-4 w-4" />
-                <p className="text-blue text-base font-medium" onClick={(e)=>{
+                <p className="text-blue text-base font-medium" onClick={(e) => {
                     router.back();
                 }}>Course</p>
                 <ChevronRightIcon className="text-text_grey_one h-4 w-4" />
@@ -108,61 +108,61 @@ courseId:courseId
             <h1 className='font-semibold text-4xl mt-5 text-white'>{data.title}</h1>
             <section className='flex mt-10 flex-row gap-10'>
                 <div className="flex flex-row gap-3 items-center">
-                <img
-                                   
-                                   className="text-blue h-8 w-8"
-                                   src="/category_icon.svg"/>
-                  
+                    <img
+
+                        className="text-blue h-8 w-8"
+                        src="/category_icon.svg" />
+
                     <p className="text-white text-xl font-normal">{data.courseCode}</p>
                 </div>
                 <div className="flex flex-row gap-3 items-center">
-                <img
-                                   
-                                   className="text-blue h-8 w-8"
-                                   src="/learning_mode.svg"/>
-                    
-                    <p className="text-white text-xl font-normal">{trainingData.filter((e)=>e.trainingModeId==data.CourseDurations[0].trainingModeId).length==0?"":trainingData.filter((e)=>e.trainingModeId==data.CourseDurations[0].trainingModeId)[0].trainingModeShortName}</p>
+                    <img
+
+                        className="text-blue h-8 w-8"
+                        src="/learning_mode.svg" />
+
+                    <p className="text-white text-xl font-normal">{trainingData.filter((e) => e.trainingModeId == data.CourseDurations[0].trainingModeId).length == 0 ? "" : trainingData.filter((e) => e.trainingModeId == data.CourseDurations[0].trainingModeId)[0].trainingModeShortName}</p>
                 </div>
                 <div className="flex flex-row gap-3 items-center">
-                <img
-                                   
-                                   className="text-blue h-8 w-8"
-                                   src="/Icon_clock.svg"/>
+                    <img
+
+                        className="text-blue h-8 w-8"
+                        src="/Icon_clock.svg" />
                     <p className="text-white text-xl font-normal">{data.CourseDurations[0].courseDuration} days</p>
                 </div>
                 <div className="flex flex-row gap-3 items-center">
-                <img
-                                   
-                                   className="text-blue h-8 w-8"
-                                   src="/rubee_icon.svg"/>
+                    <img
+
+                        className="text-blue h-8 w-8"
+                        src="/rubee_icon.svg" />
                     {
-                   data.CourseCostPlans.length!=0&& data.CourseCostPlans[0].offerId!=null?      <div className='flex flex-row'>
-                     <p className="text-white text-xl font-normal">₹ {data.CourseCostPlans[0].planPrice}/-</p>
-                     <p className="text-white line-through text-xl font-normal">₹ {data.CourseCostPlans[0].offerPrice}/-</p>
-                   </div>: <p className="text-white text-xl font-normal">₹ {data.CourseCostPlans[0].planPrice}/-</p>
+                        data.CourseCostPlans.length != 0 && data.CourseCostPlans[0].offerId != null ? <div className='flex flex-row'>
+                            <p className="text-white text-xl font-normal">₹ {data.CourseCostPlans[0].planPrice}/-</p>
+                            <p className="text-white line-through text-xl font-normal">₹ {data.CourseCostPlans[0].offerPrice}/-</p>
+                        </div> : <p className="text-white text-xl font-normal">₹ {data.CourseCostPlans[0].planPrice}/-</p>
                     }
                 </div>
             </section>
             <section className='flex flex-row items-start mt-20'>
-                <div onClick={(e)=>{
-                    handleDownload(data.courseId,data.title) 
+                <div onClick={(e) => {
+                    handleDownload(data.courseId, data.title)
                 }} className="cursor-pointer mx-auto box-border border flex flex-row gap-3  items-center p-3  border-blue border-1 bg-dark_blue rounded-2xl">
-                <img
-                                   
-                                   className="text-blue h-6 w-6"
-                                   src="/pdf_icon.svg"/>
+                    <img
+
+                        className="text-blue h-6 w-6"
+                        src="/pdf_icon.svg" />
                     <p className="text-white text-sm font-normal">Download Course Content</p>
                 </div>
             </section>
             <section className='flex flex-row items-start mt-10'>
-            <button onClick={(e)=>{
-entroll();
-            }} className="flex w-full justify-center rounded bg-blue py-3 px-10 font-medium text-white ">
-                   Entroll Now
+                <button onClick={(e) => {
+                    entroll();
+                }} className="flex w-full justify-center rounded bg-blue py-3 px-10 font-medium text-white ">
+                    Entroll Now
                 </button>
             </section>
             <section className={classNames("w-full cursor-pointer text-sm  flex flex-row mt-12 justify-center items-center  gap-7")}>
-                <div  className={index != 0 ? "text-white font-normal" : "text-blue font-medium"} onClick={(e) => setIndex(0)}>
+                <div className={index != 0 ? "text-white font-normal" : "text-blue font-medium"} onClick={(e) => setIndex(0)}>
                     Course Description
                 </div>
                 <div className='h-6 w-[1px] rounded-lg bg-grey'></div>
@@ -188,18 +188,18 @@ entroll();
 
             </section>
             <main className='mt-14 w-full'>
-               {index==0? <section>
-                    <h2 className='font-semibold text-2xl text-white'>Course Description</h2>
-                    <p className='mt-6 leading-6 font-normal text-sm text-white'>{data.description}</p>
-                </section>: index==2? < CourseScheduleComponent courseId={params.courseId[0]}/>: <section>
-                  
-                    <p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.Audience?.description??""}</p>
-                </section>}
+                {index == 0 ? <section>
+                    {/* <h2 className='font-semibold text-2xl text-white'>Course Description</h2> */}
+                    <p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.courseDetails?.description??""}</p>
+                </section> : index == 1 ?    <p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.courseDetails?.objectives?.description??""}</p> :  index == 2 ? < CourseScheduleComponent courseId={params.courseId[0]} /> : index == 3 ?  <p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.courseDetails?.audience?.description??""}</p> :index == 4 ?  <p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.courseDetails?.prerequisites?.description??""}</p> : <section>
+
+<p className='mt-6 leading-6 font-normal text-sm text-white'>{data?.courseDetails?.content?.description ?? ""}</p>
+</section>}
             </main>
 
-           
 
-      <FormComponent type='Course'/> 
+
+            <FormComponent type='Course' />
 
 
         </main>

52aeb45b57f7777816538cbd24ba0149d6e6068f
Arockiaraj19 <punithaarockiaraj1999@gmail.com>
2024-04-09 21:26:50 +0530

    fix the dublicate issue

--- src/hooks/technology_mode_hook.tsx
+++ src/hooks/technology_mode_hook.tsx
@@ -8,13 +8,19 @@ import { useEffect, useState } from 'react';
 const useTechnologyMode = () => {
   const [technologyData, setTechnology] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  function removeDuplicates(array:any, property:any) {
+    let seen:any = {};
+    return array.filter(function(item:any) {
+        let key = item[property];
+        return seen.hasOwnProperty(key) ? false : (seen[key] = true);
+    });
+  }
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         // Check if user data exists in local storage
         const response = await axiosPublic.get('/lms/course-category'); // Assuming you have an API route for user data
-        setTechnology(response.data.CourseCategory);
+        setTechnology(removeDuplicates(response.data.CourseCategory, 'categoryName'));
       } catch (error) {
         console.error('Error fetching user data:', error);
         setIsLoading(false);

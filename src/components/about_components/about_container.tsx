ca1e47c2c32d3b3a31d9321b0542a18cadfa83ec
Arockiaraj19 <punithaarockiaraj1999@gmail.com>
2024-06-11 23:37:59 +0530

    added download option in about page

--- src/components/about_components/about_container.tsx
+++ src/components/about_components/about_container.tsx
@@ -15,6 +15,8 @@ import imageHelper from '@/common/image_helper';
 import { useRouter } from 'next/router';
 import { Link } from 'react-alice-carousel';
 import { useSearchParams } from 'next/navigation';
+import LeadFormModel from '../helpers/LeadFromModel';
+import useUserData from '@/hooks/userData';
 
 export default function AboutContainer() {
   const searchParams = useSearchParams()
@@ -26,10 +28,10 @@ export default function AboutContainer() {
       name: "Overview"
     },
 
-   /* {
-      index: 2,
-      name: "Leadership"
-    },*/
+    /* {
+       index: 2,
+       name: "Leadership"
+     },*/
     {
       index: 3,
       name: "Our Client"
@@ -45,9 +47,9 @@ export default function AboutContainer() {
 
   useEffect(() => {
     fetchData();
-if(queryIndex){
-  setIndex(parseInt(queryIndex));
-}
+    if (queryIndex) {
+      setIndex(parseInt(queryIndex));
+    }
     return () => {
 
     }
@@ -74,10 +76,10 @@ if(queryIndex){
 
       // Get the document URL from the data
       const documentURL = imageHelper("/" + documentData[0].documentURL);
-      
+
       // GET the document URL
       const response = await axiosPublic.get(documentURL, {
-        responseType: 'blob' 
+        responseType: 'blob'
       });
 
       // Create a blob object from the response data
@@ -125,42 +127,60 @@ if(queryIndex){
 
 
   ];
-const router=useRouter();
+  const router = useRouter();
+  let [isOpen, setIsOpen] = useState(false)
+  function closeModal(isDownloaded: any) {
+    if (isDownloaded == true) {
+      handleDownload();
+    }
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+  const { userData, } = useUserData();
   return (
     <main className="w-full bg-primary_color flex-1 flex flex-col justify-start items-start">
 
+      <LeadFormModel data={{}} isOpen={isOpen} closeModal={closeModal} courseCode={""} courseName={""} />
+      <main className='w-full flex flex-row gap-6'>
 
-     <main className='w-full flex flex-row gap-6'>
-    
-      <section className='flex-1 flex flex-col justify-start items-start'>
-      <div className="flex flex-row gap-1 items-center">
-        <p className="cursor-pointer text-blue text-base font-medium" onClick={(e) => {
+        <section className='flex-1 flex flex-col justify-start items-start'>
+          <div className="flex flex-row gap-1 items-center">
+            <p className="cursor-pointer text-blue text-base font-medium" onClick={(e) => {
 
-          router.replace("/");
-        }}>Home</p>
-        <ChevronRightIcon className="text-text_grey_one h-4 w-4" />
-        <p className="cursor-pointer text-text_grey_one text-base font-medium">About Us</p>
-      </div>
-      <h1 className='font-semibold text-2xl mt-6 text-white'>About Us</h1>
-      <p className='text-justify mt-6 leading-6 font-normal text-[16px] text-white'>GK Cloud Solutions has created a brand in the Cloud space with specialization on Cloud Trainings across multiple brands and focusing more towards AI  with evocative approach as a Solution Provider supporting enterprises to consistently succeed in the evolving world space.</p>
+              router.replace("/");
+            }}>Home</p>
+            <ChevronRightIcon className="text-text_grey_one h-4 w-4" />
+            <p className="cursor-pointer text-text_grey_one text-base font-medium hover:text-blue">About Us</p>
+          </div>
+          <h1 className='font-semibold text-2xl mt-6 text-white'>About Us</h1>
+          <p className='text-justify mt-6 leading-6 font-normal text-[16px] text-white'>GK Cloud Solutions has created a brand in the Cloud space with specialization on Cloud Trainings across multiple brands and focusing more towards AI  with evocative approach as a Solution Provider supporting enterprises to consistently succeed in the evolving world space.</p>
 
-      <h1 className='font-semibold text-2xl mt-6 text-white'></h1>
-      <section className='flex flex-row items-start mt-3'>
-        <div onClick={() => handleDownload()} 
-          className="cursor-pointer mx-auto box-border border flex flex-row gap-3 items-center p-3 border-blue border-1 bg-dark_blue rounded-2xl">
-          <img className="text-blue h-6 w-6" src="/pdf_icon.svg" alt="PDF Icon"/>
-          <p className="text-white text-base font-normal">Download Company Profile</p>
-        </div>
-      </section>
-      </section>
-      <section className='w-1/3 h-auto'>
-      <img
+          <h1 className='font-semibold text-2xl mt-6 text-white'></h1>
+          <section className='flex flex-row items-start mt-3'>
+            <div onClick={() => {
+               if(userData){
+                handleDownload();
+            }else{
+                openModal();
+            }
+            }}
+              className="cursor-pointer mx-auto box-border border flex flex-row gap-3 items-center p-3 border-blue border-1 bg-dark_blue rounded-2xl">
+              <img className="text-blue h-6 w-6" src="/pdf_icon.svg" alt="PDF Icon" />
+              <p className="text-white text-base font-normal">Download Company Profile</p>
+            </div>
+          </section>
+        </section>
+        <section className='w-1/3 h-auto'>
+          <img
 
-className="object-contain mx-auto  h-72"
+            className="object-contain mx-auto  h-72"
 
-src="/about.png" />
-      </section>
-     </main>
+            src="/about.png" />
+        </section>
+      </main>
       <section className="mx-auto box-border w-full  mt-20  border-none border-blue grid grid-cols-4   items-center rounded-full">
         {
           items.map((e, indexx) => <div key={indexx} className={classNames("cursor-pointer py-4  w-full box-border border flex flex-row items-center justify-center text-white text-lg font-semibold border-blue border-1 ", index == e.index ? "rounded-full bg-dark_blue" : "border-none rounded-none ")} onClick={(event) => {
@@ -174,9 +194,9 @@ src="/about.png" />
       <main className='w-full mt-14 text-justify'>
         {
           data.length == 0 ? <></> : index == 0 ? <section>
-                        <p className='leading-6 font-normal text-[16px] text-white mb-6'>GK Cloud Solutions established to fill the void in Technology Training, Services & Project Consulting.</p>
-                        <p className='leading-6 font-normal text-[16px] text-white mb-6'><span className='font-semibold'>Training:</span> We are a value-driven Tech Ed Company who strives to provide unrivalled Technology continuum of training’s to enable aspiring associates to exceed their potential. We offer an end-to-end Portfolio of training services spanning multiple technologies that can be customized, built, and digitized to suit the client’s need. Pioneer in transformative technologies like AI, ML, IoT, Cloud, Data Science, Blockchain & Analytics. Reach is not a restriction to educate.</p>
-                        <p className='leading-6 font-normal text-[16px] text-white'><span className='font-semibold'>Consulting:</span> We are an Agile solution provider for Projects from Development, Consultation & Infrastructure Support. Every emerging technology goes through the scrum process and build business-centric value propositions for enabling digital transformations. Our custom-built business offerings for Offshore & Onsite curtailing costs, risk & compliance management thus augmenting workforce productivity in industries across Insurance, Banking, Retails, and Manufacturing. We are aligned to bridge the visible existing gaps between the traditional technologies (Legacy systems) through digital transformation.</p>
+            <p className='leading-6 font-normal text-[16px] text-white mb-6'>GK Cloud Solutions established to fill the void in Technology Training, Services & Project Consulting.</p>
+            <p className='leading-6 font-normal text-[16px] text-white mb-6'><span className='font-semibold'>Training:</span> We are a value-driven Tech Ed Company who strives to provide unrivalled Technology continuum of training’s to enable aspiring associates to exceed their potential. We offer an end-to-end Portfolio of training services spanning multiple technologies that can be customized, built, and digitized to suit the client’s need. Pioneer in transformative technologies like AI, ML, IoT, Cloud, Data Science, Blockchain & Analytics. Reach is not a restriction to educate.</p>
+            <p className='leading-6 font-normal text-[16px] text-white'><span className='font-semibold'>Consulting:</span> We are an Agile solution provider for Projects from Development, Consultation & Infrastructure Support. Every emerging technology goes through the scrum process and build business-centric value propositions for enabling digital transformations. Our custom-built business offerings for Offshore & Onsite curtailing costs, risk & compliance management thus augmenting workforce productivity in industries across Insurance, Banking, Retails, and Manufacturing. We are aligned to bridge the visible existing gaps between the traditional technologies (Legacy systems) through digital transformation.</p>
             {/* <p className='leading-6 font-normal text-[16px] text-white'>{data.filter((e) => e.generalId == 2)[0].description}</p> */}
           </section> : index == 2 ? <section className='w-full mx-auto flex flex-row justify-center gap-4'>
             {leaderShip.map((e, index) => <div key={index} className="relative cursor-pointer w-52 box-border border flex flex-col justify-start items-start border-blue border-1 bg-dark_blue">

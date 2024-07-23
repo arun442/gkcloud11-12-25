04269442ec84c3f8e67d1b55f070460538d67ef0
SUDNAR <farmer.tff@gmail.com>
2024-07-23 18:29:24 +0530

    Updated API for Dev Server

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl="https://i.gkcloud.ai";
+export const baseUrl="https://gate.amudhu.tech";
 export const leadBaseUrl="https://digital.globalknowledgetech.com:8143/sageapiV2"
 
 export const searchTextRegEx=/^[a-zA-Z0-9 ._\-,()\[\]]+$/
